7493449f282829ec201b28f75822f9bba5e16e85
Enockmamboleo1 <mamboleoenock@gmail.com>
2025-10-04 13:04:49 +0300

    Create node.js

---
+++ node.js
@@ -0,0 +1,9 @@
+  <!-- Embedded JavaScript -->
+  <script>
+    function buyPlan(planName, price) {
+      alert("You selected: " + planName + " - Ksh " + price);
+      // Later: Here we can integrate M-Pesa STK push or payment API
+    }
+  </script>
+</body>
+</html>
